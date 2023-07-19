b809f671939edc4be48db1617d38835d5e22b916
Michie <contact@michiewillman.com>
2023-07-18 19:06:17 -0700

    reordered middleware to fix error

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -12,12 +12,7 @@ const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
-/**
- *  Function to read data from a given a file and append some content
- *  @param {object} content The content you want to append to the file.
- *  @param {string} file The path to the file you want to save to.
- *  @returns {void} Nothing
- */
+
 const readAndAppend = (content, file) => {
   fs.readFile(file, "utf8", (err, data) => {
     if (err) {
@@ -29,5 +24,7 @@ const readAndAppend = (content, file) => {
     }
   });
 };
+
+
 
 module.exports = { readFromFile, writeToFile, readAndAppend };
--- server.js
+++ server.js
@@ -5,20 +5,20 @@ const api = require("./routes/index");
 const PORT = 3001;
 const app = express();
 
-// Middleware - Locate/use everything in 'public' dir
-app.use(express.static("public"));
-
 // Middleware - Parse JSON and urlencoded form data (boilerplate)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Middleware - Locate/use everything in 'public' dir
+app.use(express.static("public"));
 
 // Middleware - Send all the requests that begin with /api to the index.js in the routes folder
 app.use("/api", api);
 
 // GET Route for notes page
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
-);
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
 // GET Route for wildcard route / unreserved characters
 app.get("*", (req, res) =>
