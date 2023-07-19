4ae04e1f387a8a9e09db0c168478ae1204d48181
Michie <contact@michiewillman.com>
2023-07-18 19:08:31 -0700

    test notes working

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -2,12 +2,7 @@ const fs = require("fs");
 const util = require("util");
 
 const readFromFile = util.promisify(fs.readFile);
-/**
- *  Function to write data to the JSON file given a destination and some content
- *  @param {string} destination The file you want to write to.
- *  @param {object} content The content you want to write to the file.
- *  @returns {void} Nothing
- */
+
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
@@ -24,7 +19,5 @@ const readAndAppend = (content, file) => {
     }
   });
 };
-
-
 
 module.exports = { readFromFile, writeToFile, readAndAppend };
